c2e270bb1cdb3f01186489207fc22143dd69e49e
deenthusiast <161865560+deenthusiast@users.noreply.github.com>
2024-03-01 09:34:00 -0700

    Update stream_lit.py
    add Breakfast menu header
    added 3 different items available under the menu

--- stream_lit.py
+++ stream_lit.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parentss New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Bluebeery Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
