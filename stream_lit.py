8f28905c3d604914e2caa45203a62da971ed32ff
deenthusiast <161865560+deenthusiast@users.noreply.github.com>
2024-03-01 16:10:19 -0700

    Update stream_lit.py
    removed the extra text

--- stream_lit.py
+++ stream_lit.py
@@ -45,7 +45,6 @@ streamlit.header("The Fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add')
-streamlit.write('The use entered', add_my_fruit)
+add_my_fruit = streamlit.text_input('What fruit would you like to add', 'jackfruit')
 
 streamlit.write('thanks for adding', add_my_fruit)
